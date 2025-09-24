2879cdc1b65d6aa40dbc8fa556443537d382c8f8
grafst <grafst@users.noreply.github.com>
2025-09-24 08:31:25 +0200

    add german translation

---
+++ resources/lang/de/hookshelper.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'tooltip' => ':Action Hooks-Sichtbarkeit',
+    'success' => [
+        'title' => 'Hooks-Sichtbarkeit :status',
+    ],
+    'activate' => 'aktivieren',
+    'deactivate' => 'deaktivieren',
+    'activated' => 'aktiviert',
+    'deactivated' => 'deaktiviert',
+    'show' => 'Hooks anzeigen',
+    'hide' => 'Hooks ausblenden',
+];
